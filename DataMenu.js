90e3dcc80eb099032f74cd8ff8fa9809b75766c3
Iluze Ear <nsumarna150@gmail.com>
2024-08-12 23:57:14 +0700

    Menambahkan Tombol Toast series pada HTML

--- DataMenu.js
+++ DataMenu.js
@@ -57,66 +57,69 @@ const ToastSeriesArray = [
   DurianOriginal,
 ];
 
-// Membuat warp untuk Toast series
-const container = document.getElementById("container");
-const ToastSeriess = document.createElement("div");
-ToastSeriess.setAttribute("id", "ToastSeries");
-ToastSeriess.textContent = "TOAST SERIES";
-container.appendChild(ToastSeriess);
+function generateToast() {
+  // Membuat warp untuk Toast series
+  const container = document.getElementById("container");
+  const ToastSeriess = document.createElement("div");
+  ToastSeriess.setAttribute("id", "ToastSeries");
+  ToastSeriess.textContent = "TOAST SERIES";
+  container.appendChild(ToastSeriess);
 
-// Membuat Warp Untuk List name
-const WarpToastSeries = document.createElement("div");
-WarpToastSeries.setAttribute("id", "WarpMenu");
-document.getElementById("ToastSeries").appendChild(WarpToastSeries);
+  // Membuat Warp Untuk List name
+  const WarpToastSeries = document.createElement("div");
+  WarpToastSeries.setAttribute("id", "WarpMenu");
+  document.getElementById("ToastSeries").appendChild(WarpToastSeries);
 
-// Child Toast Series Flavor, Selimut Hijau, Wheat Toast, Special Toast
-// FLAVOR
-const ToastSeriesFlavor = document.createElement("div");
-ToastSeriesFlavor.setAttribute("class", "HeaderMenu");
-ToastSeriesFlavor.textContent = "FLAVOR";
-document.getElementById("WarpMenu").appendChild(ToastSeriesFlavor);
+  // Child Toast Series Flavor, Selimut Hijau, Wheat Toast, Special Toast
+  // FLAVOR
+  const ToastSeriesFlavor = document.createElement("div");
+  ToastSeriesFlavor.setAttribute("class", "HeaderMenu");
+  ToastSeriesFlavor.textContent = "FLAVOR";
+  document.getElementById("WarpMenu").appendChild(ToastSeriesFlavor);
 
-// Selimut Hijau
-const ToastSeriesSelimutHijau = document.createElement("div");
-ToastSeriesSelimutHijau.setAttribute("class", "HeaderMenu");
-ToastSeriesSelimutHijau.textContent = "SELIMUT HIJAU";
-document.getElementById("WarpMenu").appendChild(ToastSeriesSelimutHijau);
-
-// WHEAT TOAST
-const ToastSeriesWheatToast = document.createElement("div");
-ToastSeriesWheatToast.setAttribute("class", "HeaderMenu");
-ToastSeriesWheatToast.textContent = "WHEAT TOAST";
-document.getElementById("WarpMenu").appendChild(ToastSeriesWheatToast);
-
-// SPECIAL TOAST
-const ToastSeriesSpecialToast = document.createElement("div");
-ToastSeriesSpecialToast.setAttribute("class", "HeaderMenu");
-ToastSeriesSpecialToast.textContent = "SPECIAL TOAST";
-document.getElementById("WarpMenu").appendChild(ToastSeriesSpecialToast);
-
-// Child Name List
-for (x in ToastSeriesArray) {
-  // Flavor
-  const Flavor = document.createElement("div");
-  Flavor.setAttribute("class", "NameList Flavor");
-  Flavor.textContent = ToastSeriesArray[x].Flavor;
-  document.getElementById("WarpMenu").appendChild(Flavor);
-
-  // SELIMUT HIJAU
-  const SelimutHijau = document.createElement("div");
-  SelimutHijau.setAttribute("class", "NameList SelimutHijau");
-  SelimutHijau.textContent = ToastSeriesArray[x].PriceSelimutHijau;
-  document.getElementById("WarpMenu").appendChild(SelimutHijau);
+  // Selimut Hijau
+  const ToastSeriesSelimutHijau = document.createElement("div");
+  ToastSeriesSelimutHijau.setAttribute("class", "HeaderMenu");
+  ToastSeriesSelimutHijau.textContent = "SELIMUT HIJAU";
+  document.getElementById("WarpMenu").appendChild(ToastSeriesSelimutHijau);
 
   // WHEAT TOAST
-  const WheatToast = document.createElement("div");
-  WheatToast.setAttribute("class", "NameList WheatToast");
-  WheatToast.textContent = ToastSeriesArray[x].PriceWheatToast;
-  document.getElementById("WarpMenu").appendChild(WheatToast);
+  const ToastSeriesWheatToast = document.createElement("div");
+  ToastSeriesWheatToast.setAttribute("class", "HeaderMenu");
+  ToastSeriesWheatToast.textContent = "WHEAT TOAST";
+  document.getElementById("WarpMenu").appendChild(ToastSeriesWheatToast);
 
-  //SPECIAL TOAST
-  const SpecialToast = document.createElement("div");
-  SpecialToast.setAttribute("class", "NameList SpecialToast");
-  SpecialToast.textContent = ToastSeriesArray[x].PriceSpecialToast;
-  document.getElementById("WarpMenu").appendChild(SpecialToast);
+  // SPECIAL TOAST
+  const ToastSeriesSpecialToast = document.createElement("div");
+  ToastSeriesSpecialToast.setAttribute("class", "HeaderMenu");
+  ToastSeriesSpecialToast.textContent = "SPECIAL TOAST";
+  document.getElementById("WarpMenu").appendChild(ToastSeriesSpecialToast);
+
+  // Child Name List
+  for (x of ToastSeriesArray) {
+    // Flavor
+    const Flavor = document.createElement("div");
+    Flavor.setAttribute("class", "NameList Flavor");
+    Flavor.textContent = x.Flavor;
+    document.getElementById("WarpMenu").appendChild(Flavor);
+
+    // SELIMUT HIJAU
+    const SelimutHijau = document.createElement("div");
+    SelimutHijau.setAttribute("class", "NameList SelimutHijau");
+    SelimutHijau.textContent = x.PriceSelimutHijau;
+    document.getElementById("WarpMenu").appendChild(SelimutHijau);
+
+    // WHEAT TOAST
+    const WheatToast = document.createElement("div");
+    WheatToast.setAttribute("class", "NameList WheatToast");
+    WheatToast.textContent = x.PriceWheatToast;
+    document.getElementById("WarpMenu").appendChild(WheatToast);
+
+    //SPECIAL TOAST
+    const SpecialToast = document.createElement("div");
+    SpecialToast.setAttribute("class", "NameList SpecialToast");
+    SpecialToast.textContent = x.PriceSpecialToast;
+    document.getElementById("WarpMenu").appendChild(SpecialToast);
+  }
+  document.getElementById("generateToast").style.display = "none";
 }
